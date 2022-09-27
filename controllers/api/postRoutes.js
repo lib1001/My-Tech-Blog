23014eafe19191b75a308c9c3bafb830e97ce2ce
Lauren <lburke522@gmail.com>
2022-09-27 19:42:08 -0400

    added put to postroutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,17 +1,35 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require("../../models");
+const { Post } = require("../../models");
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
       ...req.body,
-      user_id: req.session.user_id,
     });
 
     res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updatedPost = await Post.update(
+      {
+        title: req.body.title,
+        post_content: req.body.post_content
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
