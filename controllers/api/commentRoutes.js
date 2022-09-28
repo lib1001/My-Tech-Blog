1011b66cba18b405676f9b436d63e8c3392150f8
Lauren <lburke522@gmail.com>
2022-09-28 09:59:26 -0400

    made commentRoutes more uniform

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,27 +2,37 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', (req,res) => {
-    Comment.findAll({})
-    .then(commentData => res.json(commentData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    });
+router.get('/', async (req,res) => {
+  try {
+    const commentData = await Comment.findAll({})
+    if (!commentData) {
+      res.status(404).json({ message: 'No comments...yet!' });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  Comment.findAll({
-          where: {
-              id: req.params.id
-          }
-      })
-      .then(commentData => res.json(commentData))
-      .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-      })
+router.get('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll(
+      {
+        where: {
+          id: req.params.id
+        },
+    });
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.post('/', withAuth, async (req, res) => {
   try {
